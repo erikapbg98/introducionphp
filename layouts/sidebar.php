a1d0042792abb9386042b8f2e0cc1d4a518955e6
erikapbg98 <eripalomita97@gmail.com>
2021-01-14 23:30:33 -0700

    plantillas

---
+++ layouts/sidebar.php
@@ -0,0 +1,45 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="../../index3.html" class="brand-link">
+     
+      <span class="brand-text font-weight-light">MI TIENDA</span>
+    </a>
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="./dashboard/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Alexander Pierce</a>
+        </div>
+      </div>
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+               <li class="nav-item">
+                <a href="./index.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Incio</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./users.php" class="nav-link">
+                  <i class="nav-icon fas fa-user"></i>
+                  <p>Usuarios</p>
+                </a>
+                <li class="nav-item">
+                <a href="../../index3.html" class="nav-link">
+                  <i class="nav-icon fas fa-list"></i>
+                  <p>Productos</p>
+                </a>
+              </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
